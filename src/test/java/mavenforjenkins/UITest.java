fdb25d0287838d214c20b51c717a924b3b614ffb
Vickey <118042499+VickeyDounde@users.noreply.github.com>
2024-01-31 18:28:04 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -11,29 +11,17 @@ import org.testng.annotations.Test;
 public class UITest 
 {
 
-	@Parameters("Browser")
+	
 	@Test
-	public void startBrowser(String browserName)
+	public void startBrowser()
 	{
 		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
-		
-		if(browserName.contains("Chrome"))
-		{
-			ChromeOptions opt=new ChromeOptions();
-			opt.addArguments("--headless");
-			opt.addArguments("--no-sandbox");
-			opt.addArguments("--disable-dev-shm-usage");
-			driver=new ChromeDriver(opt);
-		}
-		else if(browserName.contains("Edge"))
-		{
-			 driver=new EdgeDriver();
-		}
+		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
 		driver.quit();
+		
 	}
 	
 	
